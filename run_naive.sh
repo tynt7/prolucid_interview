1d075fbcfaf909a6726f297d25dc484eab7fb883
Ty Laptop <tynashtho@gmail.com>
2023-02-09 23:13:32 -0500

    cleaning up structure a bit, remove relics from debugging etc

--- run_naive.sh
+++ run_naive.sh
@@ -3,7 +3,7 @@
 # use the time command to get a naive look at how efficent I was
 time cargo run
 
-time python py_sort_naive.py
+time python ./src/py/py_sort_naive.py
 
 # compare the two naive to show method is the same
 grep -Ff outputs/rust/sent_onl_rs.txt outputs/python/sent_only_py_naive.txt
